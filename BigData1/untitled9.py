6aa2a5efc735d86d12f63483189ca5605111aafd
Andrin Iberl <andriniberl@MacBook-Pro-von-Andrin.local>
2025-02-05 10:46:56 +0100

    first update

---
+++ BigData1/untitled9.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 10:43:24 2025
+
+@author: andriniberl
+"""
+def divider(x,y):
+    divided = x/y
+    return divided
+
+divider(10,2)
